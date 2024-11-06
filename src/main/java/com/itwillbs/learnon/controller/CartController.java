db5557f31f5e3d73345bd67ead3602444b619097
bbora <bbora@bora>
2024-11-07 00:12:35 +0900

    Controller

--- src/main/java/com/itwillbs/learnon/controller/CartController.java
+++ src/main/java/com/itwillbs/learnon/controller/CartController.java
@@ -1,9 +1,15 @@
 package com.itwillbs.learnon.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import com.itwillbs.learnon.service.CartService;
 
 @Controller
 public class CartController {
+	@Autowired
+	private CartService cartService;
 	
+	//=================================================================================
 	
 }
